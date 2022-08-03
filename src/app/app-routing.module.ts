89b7472887e4bd3538e6c84652d95ab1f4244bc8
Michael9914 <michael_9914@hotmail.com>
2022-08-03 18:17:35 -0500

    fix routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  {
-    path: 'login',
-    component: LoginComponent
-  },
   { path:'', 
     redirectTo:'/login', 
     pathMatch:'full'
   },
+  {
+    path: 'login',
+    component: LoginComponent
+  }
 ];
 
 @NgModule({
