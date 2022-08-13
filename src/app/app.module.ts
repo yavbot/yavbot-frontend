2f22c573d3ff68bdafb908d4ea3166b47faa50f3
gabriel <gmb.urquizo@yavirac.edu.ec>
2022-08-13 17:22:50 -0500

    chatbot rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { ChatbotComponent} from './chatbot/chatbot.component';
 //Angular Material
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
+import {HttpClientModule} from '@angular/common/http';
 
 
 
@@ -26,7 +27,8 @@ import {MatInputModule} from '@angular/material/input';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+		HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
